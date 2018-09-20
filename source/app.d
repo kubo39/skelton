16813140d9e3293ca0b8b9a178a62b4fe76fd8df
Hiroki Noda <kubo39@gmail.com>
2018-09-21 00:59:50 +0900

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,52 @@
+import vibe.http.server;
+import vibe.http.router;
+import vibe.http.session;
+import vibe.http.fileserver;
+import vibe.core.core;
+import vibe.core.file;
+import vibe.inet.mimetypes;
+
+import mysql;
+
+import std.algorithm;
+import std.array;
+import std.conv : to;
+import std.datetime;
+import std.format : format;
+import std.process;
+import std.regex;
+import std.stdio;
+import std.string;
+import std.uuid;
+import std.variant;
+
+MySQLPool client;
+
+void getIndex(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.render!("index.dt");
+}
+
+void main()
+{
+    auto host   = environment.get("ISUCON_DB_HOST", "localhost");
+    auto port   = environment.get("ISUCON_DB_PORT", "3306");
+    auto user   = environment.get("ISUCON_DB_USER", "root");
+    auto pwd    = environment.get("ISUCON_DB_PASSWORD", "password");
+    auto dbname = environment.get("ISUCON_DB_NAME", "isucon");
+    auto dsn = format("host=%s;port=%s;user=%s;pwd=%s;db=%s",
+                      host, port, user, pwd, dbname);
+    client = new MySQLPool(dsn);
+
+    auto router = new URLRouter;
+    router.get("/", &getIndex);
+
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.sessionStore = new MemorySessionStore;
+
+    listenHTTP(settings, router);
+
+    runApplication();
+}
